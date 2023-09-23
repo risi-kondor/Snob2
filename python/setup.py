56d801949ae8eff87a2049949d728fb72cf7f13c
Risi Kondor <risi@cs.uchicago.edu>
2023-09-23 17:08:21 -0500

    'changes'

--- python/setup.py
+++ python/setup.py
@@ -22,7 +22,7 @@ snob2_root=".."
 # ------------------------------------------------------------------------------------------------------------
 
 
-cwd = os.getcwd()
+cwd = os.getcwd()+"/"
 
 _cxx_compile_args = ['-std=c++17',
 		     '-Wno-sign-compare',
@@ -39,17 +39,17 @@ _cxx_compile_args = ['-std=c++17',
 		     ]
 
 ext_modules=[CppExtension('snob_base', ['bindings/Snob2_bindings.cpp'],
-			  include_dirs=[cnine_root,
-					cnine_root+'/include',
-					cnine_root+'/modules',
-					cnine_root+'/combinatorial',
-					cnine_root+'/math',
-					cnine_root+'/objects/tensor_views',
-					cnine_root+'/objects/tensor_views/functions',
+			  include_dirs=[cwd+cnine_root,
+					cwd+cnine_root+'/include',
+					cwd+cnine_root+'/modules',
+					cwd+cnine_root+'/combinatorial',
+					cwd+cnine_root+'/math',
+					cwd+cnine_root+'/objects/tensor_views',
+					cwd+cnine_root+'/objects/tensor_views/functions',
 
-					snob2_root+'/include',
-					snob2_root+'/combinatorial',
-					snob2_root+'/Sn'],
+					cwd+snob2_root+'/include',
+					cwd+snob2_root+'/combinatorial',
+					cwd+snob2_root+'/Sn'],
 			  extra_compile_args = {'cxx': _cxx_compile_args},
 			  depends=['setup.py',
 				   'bindings/Snob2_bindings.cpp',
